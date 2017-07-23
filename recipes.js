56a59cd2cd576406f5631273966980fc0394f3aa
Georgia Tardy <georgiatardyart@gmail.com>
2017-07-23 18:00:11 -0400

    began writing js file

---
+++ recipes.js
@@ -0,0 +1,34 @@
+let search = document.getElementById('button_box')
+.addEventListener('click', onSearch);
+
+function onSearch() {
+  let content = document.getElementById(content);
+  let input_box = document.getElementById('inout_box').value;
+
+}
+
+// Gets API data
+fetch(
+  http:'http://crossorigin.me/http://www.recipepuppy.com/api/?q=${search-term}'
+)
+
+.then(function(response) {
+  if (response.status !==200){
+    console.log(resoonse.status);
+    return;
+  }
+
+  content.innerHTML = ''
+  response.json().then(function(data){
+    data.results.forEach(function(results) {
+
+      if (results.thumbnail==='') {
+        results.thumbnail = "http://via.placeholder.com/150x150";
+      }
+
+      let template = <article class="recipe">
+      <a href="${results.href}">
+    }
+
+  }
+}
